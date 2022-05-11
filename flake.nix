0d5f1b4a05e3f6a1851b3bffb3053f219c926075
Tom Bereknyei <tomberek@gmail.com>
2022-05-11 17:44:27 -0400

    feat: add recurseForDerivations everywhere
    This is due to https://github.com/NixOS/nix/blob/master/src/nix/search.cc#L159
    
    TODO: remove this and use mapAttrRecursive to insert this via capacitor

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 rec {
+  inputs.nixpkgs.url = "github:flox/nixpkgs/stable";
   inputs.nixpkgs-stable.url = "github:flox/nixpkgs/stable";
   inputs.nixpkgs-unstable.url = "github:flox/nixpkgs/unstable";
   inputs.nixpkgs-staging.url = "github:flox/nixpkgs/staging";
@@ -11,18 +12,38 @@ rec {
   outputs = {self, capacitor, ...} @ args: (capacitor args ({auto,...}: rec {
     packages = auto.automaticPkgsWith inputs ./pkgs;
     legacyPackages = {system,...}: rec {
-      nixpkgs.stable = args.nixpkgs-stable.legacyPackages.${system};
-      nixpkgs.unstable = args.nixpkgs-unstable.legacyPackages.${system};
-      nixpkgs.staging = args.nixpkgs-staging.legacyPackages.${system};
+      # TODO: fold into capacitor
+      nixpkgs.recurseForDerivations = true;
+      nixpkgs.stable = args.nixpkgs-stable.legacyPackages.${system} // {
+        recurseForDerivations = true;
+      };
+      nixpkgs.unstable = args.nixpkgs-unstable.legacyPackages.${system} // {
+        recurseForDerivations = true;
+      };
+      nixpkgs.staging = args.nixpkgs-staging.legacyPackages.${system} // {
+        recurseForDerivations = true;
+      };
 
-      flox.unstable = packages nixpkgs.unstable;
-      flox.stable = packages nixpkgs.stable;
-      flox.staging = packages nixpkgs.staging;
+      flox.recurseForDerivations = true;
+      flox.unstable = packages nixpkgs.unstable
+      // {
+        recurseForDerivations = true;
+      };
+      flox.stable = packages nixpkgs.stable
+      // {
+        recurseForDerivations = true;
+      };
+      flox.staging = packages nixpkgs.staging
+      // {
+        recurseForDerivations = true;
+      };
 
       cached.nixpkgs.stable = if system == "x86_64-linux"
                               then args.cached__nixpkgs-stable__x86_64-linux.legacyPackages.${system}
                               else {};
     };
+
+    # apps = auto.automaticPkgsWith inputs ./apps ;
   }));
 }
 
