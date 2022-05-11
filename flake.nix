aafb09d7e258a69f9de455789e64a288b6c8b87b
Tom Bereknyei <tomberek@gmail.com>
2022-05-11 10:49:18 -0400

    fix: restructure namespace for <channel>.<stability>.<attrPath>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,32 @@
 rec {
-  inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor";
+  inputs.nixpkgs-stable.url = "github:flox/nixpkgs/stable";
+  inputs.nixpkgs-unstable.url = "github:flox/nixpkgs/unstable";
+  inputs.nixpkgs-staging.url = "github:flox/nixpkgs/staging";
 
-  outputs = {self, capacitor, ...} @ args: capacitor args ({auto,...}: {
+  inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor?ref=ysndr";
+  inputs.capacitor.inputs.nixpkgs.follows = "nixpkgs-unstable";
+
+  outputs = {self, capacitor, ...} @ args: (capacitor args ({auto,...}: rec {
     packages = auto.automaticPkgsWith inputs ./pkgs;
-    legacyPackages = capacitor.inputs.nixpkgs.legacyPackages;
-  });
+    legacyPackages = {system,...}: rec {
+      nixpkgs.stable = args.nixpkgs-stable.legacyPackages.${system};
+      nixpkgs.unstable = args.nixpkgs-unstable.legacyPackages.${system};
+      nixpkgs.staging = args.nixpkgs-staging.legacyPackages.${system};
+
+      flox.unstable = packages nixpkgs.unstable;
+      flox.stable = packages nixpkgs.stable;
+      flox.staging = packages nixpkgs.staging;
+    };
+  }));
 }
+
+    /*
+    Produce a namespace with the following structure:
+    legacyPackages.<system>.<channel>.<stability>.<attrPath>
+    eg:
+    legacyPackages.<system>.nixpkgs.stable.hello
+    legacyPackages.<system>.nixpkgs.stable.python3Packages.requests
+    legacyPackages.<system>.flox.stable.tracelinks
+    legacyPackages.<system>.flox.stable.python3Packages.hello-python-library
+    TODO: make a helper for this: has.stabilities ["stable" "unstable" "staging"]
+    */
