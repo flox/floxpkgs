d14d6be28ae5cef9468619c874e945a93edfd159
Tom Bereknyei <tomberek@gmail.com>
2022-05-12 12:04:27 -0400

    fix: remove tracelinks from packages
    tracelinks is not reliable due to requiring out-of-band access to source
    code. Don't place it into packages, only legacyPackages which is more
    forgiving during operations like search.

--- flake.nix
+++ flake.nix
@@ -1,42 +1,30 @@
 rec {
-  inputs.nixpkgs.url = "github:flox/nixpkgs/stable";
+  inputs.lib.url = "github:nix-community/nixpkgs.lib";
+
   inputs.nixpkgs-stable.url = "github:flox/nixpkgs/stable";
   inputs.nixpkgs-unstable.url = "github:flox/nixpkgs/unstable";
   inputs.nixpkgs-staging.url = "github:flox/nixpkgs/staging";
 
   inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor?ref=ysndr";
   inputs.capacitor.inputs.nixpkgs.follows = "nixpkgs-unstable";
+  inputs.capacitor.inputs.root.follows = "/";
 
   inputs.cached__nixpkgs-stable__x86_64-linux.url = "https://hydra.floxsdlc.com/channels/nixpkgs/stable/x86_64-linux.tar.gz";
 
   outputs = {self, capacitor, ...} @ args: (capacitor args ({auto,...}: rec {
-    packages = auto.automaticPkgsWith inputs ./pkgs;
     legacyPackages = {system,...}: rec {
       # TODO: fold into capacitor
-      nixpkgs.recurseForDerivations = true;
-      nixpkgs.stable = args.nixpkgs-stable.legacyPackages.${system} // {
-        recurseForDerivations = true;
-      };
-      nixpkgs.unstable = args.nixpkgs-unstable.legacyPackages.${system} // {
-        recurseForDerivations = true;
-      };
-      nixpkgs.staging = args.nixpkgs-staging.legacyPackages.${system} // {
-        recurseForDerivations = true;
-      };
+      nixpkgs = args.lib.lib.recurseIntoAttrs (builtins.mapAttrs (_: x: args.lib.lib.recurseIntoAttrs x) {
+        stable   = args.nixpkgs-stable.legacyPackages.${system};
+        unstable = args.nixpkgs-unstable.legacyPackages.${system};
+        staging  = args.nixpkgs-staging.legacyPackages.${system};
+      });
 
-      flox.recurseForDerivations = true;
-      flox.unstable = packages nixpkgs.unstable
-      // {
-        recurseForDerivations = true;
-      };
-      flox.stable = packages nixpkgs.stable
-      // {
-        recurseForDerivations = true;
-      };
-      flox.staging = packages nixpkgs.staging
-      // {
-        recurseForDerivations = true;
-      };
+      flox = args.lib.lib.recurseIntoAttrs (builtins.mapAttrs (_: x: args.lib.lib.recurseIntoAttrs x) {
+        unstable = auto.automaticPkgsWith inputs ./pkgs nixpkgs.unstable;
+        stable = auto.automaticPkgsWith inputs ./pkgs nixpkgs.stable;
+        staging = auto.automaticPkgsWith inputs ./pkgs nixpkgs.staging;
+      });
 
       cached.nixpkgs.stable = if system == "x86_64-linux"
                               then args.cached__nixpkgs-stable__x86_64-linux.legacyPackages.${system}
@@ -46,14 +34,3 @@ rec {
     # apps = auto.automaticPkgsWith inputs ./apps ;
   }));
 }
-
-    /*
-    Produce a namespace with the following structure:
-    legacyPackages.<system>.<channel>.<stability>.<attrPath>
-    eg:
-    legacyPackages.<system>.nixpkgs.stable.hello
-    legacyPackages.<system>.nixpkgs.stable.python3Packages.requests
-    legacyPackages.<system>.flox.stable.tracelinks
-    legacyPackages.<system>.flox.stable.python3Packages.hello-python-library
-    TODO: make a helper for this: has.stabilities ["stable" "unstable" "staging"]
-    */
