b2f41343ac9b724ea6a8ff1258b7a04cb53cfefd
Tom Bereknyei <tomberek@gmail.com>
2022-05-13 08:36:40 -0400

    feat: simpler recurseIntoAttrs2

--- flake.nix
+++ flake.nix
@@ -14,38 +14,44 @@ rec {
   inputs.tracelinks.url = "git+ssh://git@github.com/flox/tracelinks?ref=main";
   inputs.tracelinks.flake = false;
 
-  outputs = {self, capacitor, ...} @ args: (capacitor args ({auto,...}: rec {
-    legacyPackages = {system,...}: rec {
-      # TODO: fold into capacitor
-      tracelinks = args.tracelinks;
-      nixpkgs = args.lib.lib.recurseIntoAttrs (builtins.mapAttrs (_: x: args.lib.lib.recurseIntoAttrs x) {
-        stable   = args.nixpkgs-stable.legacyPackages.${system};
-        unstable = args.nixpkgs-unstable.legacyPackages.${system};
-        staging  = args.nixpkgs-staging.legacyPackages.${system};
-      });
+  outputs = {
+    self,
+    capacitor,
+    ...
+  } @ args:
+    capacitor args ({auto, ...}: rec {
+      legacyPackages = {system, ...}: rec {
+        nixpkgs = capacitor.lib.recurseIntoAttrs2 {
+          stable = args.nixpkgs-stable.legacyPackages.${system};
+          unstable = args.nixpkgs-unstable.legacyPackages.${system};
+          staging = args.nixpkgs-staging.legacyPackages.${system};
+        };
 
-      flox = args.lib.lib.recurseIntoAttrs (builtins.mapAttrs (_: x: args.lib.lib.recurseIntoAttrs x) {
-        unstable = auto.automaticPkgsWith inputs ./pkgs nixpkgs.unstable;
-        stable = auto.automaticPkgsWith inputs ./pkgs nixpkgs.stable;
-        staging = auto.automaticPkgsWith inputs ./pkgs nixpkgs.staging;
-      });
+        flox = capacitor.lib.recurseIntoAttrs2 {
+          unstable = auto.automaticPkgsWith inputs ./pkgs nixpkgs.unstable;
+          stable = auto.automaticPkgsWith inputs ./pkgs nixpkgs.stable;
+          staging = auto.automaticPkgsWith inputs ./pkgs nixpkgs.staging;
+        };
 
-      cached.nixpkgs.stable = if system == "x86_64-linux"
-                              then args.cached__nixpkgs-stable__x86_64-linux.legacyPackages.${system}
-                              else {};
+        cached.nixpkgs.stable =
+          if system == "x86_64-linux"
+          then args.cached__nixpkgs-stable__x86_64-linux.legacyPackages.${system}
+          else {};
 
-      search.nixpkgs.recurseForDerivations = true;
-      search.nixpkgs.stable = args.lib.lib.recurseIntoAttrs (capacitor.lib.mapAttrsRecursiveCond
-        (path: a: (a?element))
-        (path: value: {
-          type="derivation";
-          name = builtins.concatStringsSep "." (args.lib.lib.init path);
-          meta.description = "";
-          version = "";
-        })
-            self.legacyPackages.x86_64-linux.nixpkgs.stable);
-    };
+        search = capacitor.lib.recurseIntoAttrs2 {
+          nixpkgs.stable =
+            capacitor.lib.mapAttrsRecursiveCond
+            (path: a: (a ? element))
+            (path: value: {
+              type = "derivation";
+              name = builtins.concatStringsSep "." (args.lib.lib.init path);
+              meta.description = "";
+              version = "";
+            })
+            self.legacyPackages.x86_64-linux.nixpkgs.stable;
+        };
+      };
 
-    # apps = auto.automaticPkgsWith inputs ./apps ;
-  }));
+      # apps = auto.automaticPkgsWith inputs ./apps ;
+    });
 }
