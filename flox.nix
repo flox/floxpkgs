881a635755992f045f932b9f469e62ac0353caa8
Yannik Sander <yannik@floxdev.com>
2022-06-29 14:04:52 +0200

    Use minicapacitor to declare the flake

--- flox.nix
+++ flox.nix
@@ -1,3 +1,32 @@
-{_}: {
-  # External proto-derivation trees
+{inputs, ...}:
+# Define package set structure
+{
+  # Limit the systems to fewer or more than default by ucommenting
+  packages = {
+    flox = {inputs, lib, ...}: inputs.flox.defaultPackage;
+  };
+
+  apps = {
+    test = {
+      type= "app";
+      program = "";
+    };
+  };
+  
+
+  config = {
+    systems = [ "x86_64-linux" ];
+
+    stabilities = {
+      stable = inputs.nixpkgs.inputs.nixpkgs-stable;
+      staging = inputs.nixpkgs.inputs.nixpkgs-staging;
+      unstable = inputs.nixpkgs.inputs.nixpkgs-unstable;
+      default = inputs.nixpkgs.inputs.nixpkgs-stable;
+    };
+
+    extraPlugins = [
+      (inputs.capacitor.plugins.allLocalResources {})
+      (inputs.capacitor.plugins.templates {})
+    ];
+  };
 }
