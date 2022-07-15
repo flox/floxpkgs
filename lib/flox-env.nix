414dc3b275b29425e1172b9cc347097a7a0f1ae9
Yannik Sander <yannik@floxdev.com>
2022-07-15 11:18:40 +0200

    Fix: mach-nix definition
    fix: use coherent pkgs in mach-nix composition
    From capacitor/production/mach-nix-pkgs

--- lib/flox-env.nix
+++ lib/flox-env.nix
@@ -17,29 +17,39 @@ pkgs': toml: pins: let
     attrs = if builtins.isAttrs toml then toml else builtins.fromTOML (builtins.readFile toml);
   };
   pkgs = tie.pkgs;
-  floxEnv = {programs, ...}: let
-    python = floxpkgs.inputs.mach-nix.lib.${pkgs.system}.mkPython (programs.python // {
-      ignoreDataOutdated = true;
-    });
-    handler = {
-      python = python;
-      vscode =
-        floxpkgs.lib.vscode.configuredVscode
-        pkgs
-        programs.vscode
-        pins.vscode-extensions;
+    floxEnv = {programs, ...}: let
+    python = floxpkgs.inputs.mach-nix.lib.${pkgs.system}.mkPython programs.python;
+    python = let
+      mach = import (floxpkgs.inputs.mach-nix + "/default.nix") {
+        inherit pkgs;
+        dataOutdated = false;
+        pypiData = floxpkgs.inputs.mach-nix.inputs.pypi-deps-db;
+      };
+    in
+      mach.mkPython (programs.python
+        // {
+          ignoreDataOutdated = true;
+        });
+    paths = let
+      handler = {
+        python = python;
+        vscode =
+          lib.vscode.configuredVscode
+          pkgs
+          programs.vscode
+          pins.vscode-extensions;
 
-      # insert excpetions here
-      __functor = self: key: attr:
-        self.${key}
-        or (
-          if attr ? version
-          then "${key}@${attr.version}"
-          else pkgs.${key}
-        );
-    };
-    paths = lib.mapAttrsToList handler programs;
-    elements = lib.mapAttrsToList (name: value: { attr = name; value = handler name value;}) programs;
+        # insert excpetions here
+        __functor = self: key: attr:
+          self.${key}
+          or (
+            if attr ? version
+            then "${key}@${attr.version}"
+            else pkgs.${key}
+          );
+      };
+    in
+      lib.mapAttrsToList handler programs;
   in
     (pkgs.buildEnv {
       name = "flox-env";
@@ -47,7 +57,7 @@ pkgs': toml: pins: let
     })
     // {
       passthru = {
-        inherit programs paths elements;
+        inherit programs paths;
         python.expr = python.expr;
       };
     };
