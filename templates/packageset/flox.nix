864c87b5f49d6cfb3067e642e6bdaf0e329c12fc
Sam Rose <sam@floxdev.com>
2022-07-18 16:54:14 -0400

    proposed new packageset template

---
+++ templates/packageset/flox.nix
@@ -0,0 +1,21 @@
+{inputs, ...}:
+# Define package set structure
+{
+  # Limit the systems to fewer or more than default by ucommenting
+  packages = {
+
+  };
+
+  config = {
+    stabilities = {
+      stable = inputs.nixpkgs.stable;
+      staging = inputs.nixpkgs.staging;
+      unstable = inputs.nixpkgs.unstable;
+      default = inputs.nixpkgs.stable;
+    };
+
+    extraPlugins = [
+      (inputs.capacitor.plugins.allLocalResources {})
+    ];
+  };
+}
