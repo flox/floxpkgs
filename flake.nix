d0eead932c9131d87dcb838c415fcde05be75db4
Sam Rose <sam@floxdev.com>
2022-08-16 11:33:52 -0400

    movinging builtfilter to public floxpkgs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@ rec {
 
   inputs.flox.url = "git+ssh://git@github.com/flox/flox?ref=tng";
   inputs.flox.flake = false;
-  
+
   inputs.nix-editor.url = "github:vlinkz/nix-editor";
 
   inputs.devshell.url = "github:numtide/devshell";
@@ -18,7 +18,8 @@ rec {
   inputs.mach-nix.inputs.pypi-deps-db.follows = "pypi-deps-db";
   inputs.pypi-deps-db.url = "github:DavHau/pypi-deps-db";
   inputs.pypi-deps-db.flake = false;
-  
+  inputs.builtfilter.url = "git+ssh://git@github.com/flox/builtfilter?ref=builtfilter-rs";
+  inputs.builtfilter.inputs.capacitor.follows = "capacitor";
   # =================================
 
   nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
