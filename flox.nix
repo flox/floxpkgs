fda9b886eb25856bd44c6cf1fc581e7e5247e7ee
Yannik Sander <yannik@floxdev.com>
2022-08-24 15:23:15 +0200

    Reexport nixpkgs catalog

--- flox.nix
+++ flox.nix
@@ -1,4 +1,4 @@
-{inputs, ...}:
+{inputs, lib,...}:
 # Define package set structure
 {
   # Limit the systems to fewer or more than default by ucommenting
@@ -15,10 +15,17 @@
     };
 
     extraPlugins = [
-      (inputs.flox-extras.plugins.catalog {catalogDirectory = inputs.catalog + "/render/x86_64-linux"; system = "x86_64-linux";})
+      (inputs.flox-extras.plugins.catalog { 
+        catalogDirectory = inputs.catalog + "/render/x86_64-linux"; 
+        system = "x86_64-linux";
+        path = ["floxpkgs"];
+      })
       (inputs.capacitor.plugins.allLocalResources {})
       (inputs.capacitor.plugins.templates {})
       (inputs.capacitor.plugins.nixpkgs)
     ];
   };
+
+  passthru.catalog = (lib.mapAttrs (system: catalogForSystem: {nixpkgs = catalogForSystem;}) inputs.nixpkgs.catalog);
+
 }
