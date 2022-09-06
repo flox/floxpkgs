71462807b13181fac6a7c900777bf49039dd800b
Sam Rose <sam@floxdev.com>
2022-09-06 15:14:55 -0400

    elixir template

---
+++ templates/elixir/pkgs/default.nix
@@ -0,0 +1,41 @@
+{ lib, beam, ...}:
+
+let
+  # beam.interpreters.erlangR23 is available if you need a particular version
+  packages = beam.packagesWith beam.interpreters.erlang;
+
+  pname = "my-package";
+  version = "0.0.0";
+
+  src = ../.;
+
+  MIX_ENV="prod";
+  # flox will create a "fixed output derivation" based on
+  # the total package of fetched mix dependencies
+  mixFodDeps = packages.fetchMixDeps {
+    pname = "mix-deps-${pname}";
+    inherit src version;
+    # flox will complain and tell you the right sha256 value to replace this with
+    sha256 = lib.fakeSha256;
+    #sha256 = "";
+  };
+
+  configurePhase = ''
+    runHook preConfigure
+    mix deps.compile --no-deps-check
+    runHook postConfigure
+  '';
+
+in packages.mixRelease {
+  inherit src pname version mixFodDeps MIX_ENV;
+  # for phoenix framework you can uncomment the lines below
+  #postBuild = ''
+
+    # for external task you need a workaround for the no deps check flag
+    # https://github.com/phoenixframework/phoenix/issues/2690
+    #mix do deps.loadpaths --no-deps-check, phx.digest
+    #mix phx.digest --no-deps-check
+   # mix do deps.loadpaths --no-deps-check
+  #'';
+}
+
