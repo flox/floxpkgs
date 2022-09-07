a48d8d5a482d6237db1cc1f7e442085e769ba160
Sam Rose <sam@floxdev.com>
2022-09-07 15:48:32 -0400

    feat: ruby template

---
+++ templates/ruby/pkgs/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, bundlerEnv, ruby }:
+let
+  # the magic which will include gemset.nix
+  gems = bundlerEnv {
+    name = "my-package-env";
+    inherit ruby;
+    gemfile = ../Gemfile;
+    lockfile = ../Gemfile.lock;
+    gemset = ../gemset.nix;
+  };
+in stdenv.mkDerivation rec {
+  name = "my-package";
+  src = ../.;
+  buildInputs = [gems ruby];
+  installPhase = ''
+    mkdir -p $out/bin  $out/share/${name}
+    cp -r * $out/share/${name}
+    bin=$out/bin/${name}
+# we are using bundle exec to start in the bundled environment
+    cat > $bin <<EOF
+#!/bin/sh -e
+exec ${gems}/bin/bundle exec ${ruby}/bin/ruby $out/share/${name}/lib/${name}.rb "\$@"
+EOF
+    chmod +x $bin
+  '';
+}
