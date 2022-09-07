f716a1ba187b0493ba4e40acf457d24335af688c
Sam Rose <sam@floxdev.com>
2022-09-07 11:23:48 -0400

    fix: more styling requests

--- templates/elixir/pkgs/default.nix
+++ templates/elixir/pkgs/default.nix
@@ -1,35 +1,38 @@
-{ lib, beam, ...}:
-
-let
+{
+  lib,
+  beam,
+  ...
+}: let
   # beam.interpreters.erlangR23 is available if you need a particular version
   packages = beam.packagesWith beam.interpreters.erlang;
+in
+  packages.mixRelease rec {
+    MIX_ENV = "prod";
 
-  pname = "my-package";
-  version = "0.0.0";
+    pname = "my-package";
+    version = "0.0.0";
 
-  src = ../.;
+    src = ../.;
 
-  # flox will create a "fixed output derivation" based on
-  # the total package of fetched mix dependencies
-  mixFodDeps = packages.fetchMixDeps {
-    pname = "mix-deps-${pname}";
-    inherit src version;
-    # flox will complain and tell you the right sha256 value to replace this with
-    sha256 = lib.fakeSha256;
-    #sha256 = "";
-  };
+    # flox will create a "fixed output derivation" based on
+    # the total package of fetched mix dependencies
+    mixFodDeps = packages.fetchMixDeps {
+      inherit version src;
+      pname = "mix-deps-${pname}";
+      # nix will complain and tell you the right value to replace this with
+      #sha256 = lib.fakeSha256;
+      sha256 = "sha256-CYOhCqyuH2PtI7dAED+gqn1IJRbfYZZlSeG1wWtwJFs=";
+      # if you have build time environment variables add them here
+      #MY_VAR="value";
+    };
 
-in packages.mixRelease {
-  MIX_ENV="prod";
-  inherit src pname version mixFodDeps;
-  # for phoenix framework you can uncomment the lines below
-  #postBuild = ''
+    #postBuild = ''
 
+    # for phoenix framework you can uncomment the lines below
     # for external task you need a workaround for the no deps check flag
     # https://github.com/phoenixframework/phoenix/issues/2690
     #mix do deps.loadpaths --no-deps-check, phx.digest
     #mix phx.digest --no-deps-check
-   # mix do deps.loadpaths --no-deps-check
-  #'';
-}
-
+    # mix do deps.loadpaths --no-deps-check
+    #'';
+  }
