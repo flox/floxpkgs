771277592245fa88f03b40efb65086c30190c090
Sam Rose <sam@floxdev.com>
2022-09-15 13:34:03 -0400

    chore: genericize the expression

--- templates/buildBazelPackage/pkgs/default.nix
+++ templates/buildBazelPackage/pkgs/default.nix
@@ -3,17 +3,14 @@
   buildBazelPackage,
 }:
 buildBazelPackage rec {
-  # you can git clone https://github.com/bazelbuild/examples
-  # and cd into examples/cpp-tutorial/stage1 to use this template
-  # with a real bazel project example
-  pname = "hello-world";
+  pname = "my-package";
   version = "0.0.0";
-  src = ./.;
-  bazelTarget = "main:hello-world";
+  src = ../.;
+  bazelTarget = "main:my-package";
   buildAttrs = {
     installPhase = ''
       mkdir -p "$out/bin"
-      install bazel-out/k8-fastbuild/bin/main/hello-world "$out/bin"
+      install bazel-out/k8-fastbuild/bin/main/my-package "$out/bin"
     '';
   };
   fetchAttrs = {
