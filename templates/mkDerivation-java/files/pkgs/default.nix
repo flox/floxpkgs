d2b6218a3fa0a29f5f666498de989ba38bb1589c
samrose <sam@floxdev.com>
2022-09-15 14:40:14 -0400

    feat: templates/java adding java template (#42)
    * feat: templates/java adding java template
    
    * fix: formatting
    
    * feat: mkDerivation-java example
    
    * fix: conform to new file layout

---
+++ templates/mkDerivation-java/files/pkgs/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  jdk,
+  ant,
+  makeWrapper,
+}:
+stdenv.mkDerivation rec {
+  pname = "my-package";
+  version = "0.0.0";
+  src = ../.;
+  nativeBuildInputs = [ant jdk makeWrapper];
+
+  dontConfigure = true;
+
+  buildPhase = ''
+    runHook preBuild
+    ant
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin $out/share/${pname}
+    mv target/${pname}.jar $out/share/${pname}
+    makeWrapper ${jdk}/bin/java $out/bin/${pname} \
+    --add-flags "-jar $out/share/${pname}/${pname}.jar"
+    runHook postInstall
+
+  '';
+}
