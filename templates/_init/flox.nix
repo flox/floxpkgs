d3996b3fb502acbe8ad3e7ea13ee1b8b93df8796
Yannik Sander <yannik@floxdev.com>
2022-09-19 18:34:53 +0200

    fix!: use correct catalog plugin

--- templates/_init/flox.nix
+++ templates/_init/flox.nix
@@ -7,14 +7,12 @@
 # Define package set structure
 {
 
-  config.owner = "USER"; # < change this value to match your namespace
-
   # Template Configuration:
   # DO NOT EDIT
   config.extraPlugins = [
       (
-        inputs.flox-extras.plugins.allCatalogs {
-          catalogsDirectory = self.outPath + "/catalog";
+        inputs.flox-extras.plugins.catalog {
+          catalogDirectory = self.outPath + "/catalog";
         }
       )
       (inputs.capacitor.plugins.allLocalResources {})
