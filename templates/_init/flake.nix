b4dac1752d6e0feb8754c3740be93616d39d7f8f
Yannik Sander <yannik@floxdev.com>
2022-09-21 12:54:34 +0200

    Clean up init template

--- templates/_init/flake.nix
+++ templates/_init/flake.nix
@@ -7,17 +7,13 @@
   # =================================
 
 
+  description = "Floxpkgs/Project Template";
+  nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
 
   # Template DO NOT EDIT
   # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  description = "Floxpkgs/Project Template";
-  outputs = args @ {capacitor, ...}: capacitor args (import ./flox.nix);
-  nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
-  # could be inferred?
-  inputs.floxpkgs.url = "github:flox/floxpkgs";
-  inputs.capacitor.url = "github:flox/capacitor?ref=v0";
-  inputs.capacitor.inputs.root.follows = "/";
-  inputs.flox-extras.url = "github:flox/flox-extras";
-  inputs.flox-extras.inputs.capacitor.follows = "capacitor";
+  # TODO: injected by the cli, or used via registry?
+  inputs.floxpkgs.url = "github:flox/floxpkgs/tng";
+  outputs = args @ {floxpkgs, ...}: floxpkgs.capacitor args (import ./flox.nix);
   # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
