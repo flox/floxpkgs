8ba06e3cc2101452561adc59da38532f8c1cfc12
Sam Rose <sam@floxdev.com>
2022-10-11 17:36:07 -0400

    fix: template env var setting

--- templates/buildPythonPackage/files/pkgs/default.nix
+++ templates/buildPythonPackage/files/pkgs/default.nix
@@ -3,7 +3,7 @@ python3Packages.buildPythonPackage {
   pname = "my-package";
   version = "0.0.0";
   src = self; # + "/src";
-
+  PIP_DISABLE_PIP_VERSION_CHECK = 1;
   # Add Python modules needed by your package here
   propagatedBuildInputs = with python3Packages; [
     requests
