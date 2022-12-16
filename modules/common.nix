ddb8277bed4f86e62260d26044169fdb85b26b58
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2022-12-16 11:08:33 -0700

    feat: floxEnv: support flakes, store paths, and nested attributes
    The new format supports specifying packages in the form:
    packages = {
      nixpkgs-flox.hello = {};
      "github.com/nixos/nix".python3Packages.requests = {};
      "/nix/store/XXX-pkg-0.0.0" = {};
    }
    
    Flakes are fetched every time a floxEnv is built (until readPackage is
    integrated), and storePaths are assumed to be present in the store

--- modules/common.nix
+++ modules/common.nix
@@ -71,152 +71,279 @@
       then {}
       else builtins.fromJSON (builtins.readFile config.catalogPath);
 
-    getCatalogPath = channelName: packageName: packageConfig: [
-      channelName
-      system
-      packageConfig.stability or "stable"
-      packageName
-      packageConfig.version or "latest"
-    ];
+    # path getters
+    # for channel and flake - a channel has an evalCatalog while a flake does not
+    # for catalog and flake - the catalog path is the JSON path in catalog.json, and the flake path is the attribute path to the derivation
+    getChannelCatalogPath = channelName: packageAttrPath: packageConfig:
+      [
+        channelName
+        system
+        packageConfig.stability or "stable"
+      ]
+      ++ packageAttrPath
+      ++ [
+        packageConfig.version or "latest"
+      ];
 
-    getEvalCatalogPath = packageName: packageConfig: let
+    getChannelFlakePath = packageAttrPath: packageConfig: let
       version =
         if packageConfig ? version
-        then
+        then [
           builtins.replaceStrings
           ["."]
           ["_"]
           packageConfig.version
-        else "latest";
-    in [
-      system
-      packageConfig.stability or "stable"
-      packageName
-      version
-    ];
+        ]
+        # TODO confirm we don't need latest
+        else [];
+    in
+      [
+        "evalCatalog"
+        system
+        packageConfig.stability or "stable"
+      ]
+      ++ packageAttrPath
+      ++ version;
 
-    newFakeCatalog =
-      builtins.mapAttrs generateChannelFakeCatalog config.packages;
+    getFlakeCatalogPath = channelName: packageAttrPath: _:
+      [
+        channelName
+        system
+      ]
+      ++ packageAttrPath;
 
-    generateChannelFakeCatalog = channelName: channelPackages: let
-      # utility function - should be in lib?
-      attrsToList = attrSet:
-        map (name: {
-          inherit name;
-          value = attrSet.${name};
-        }) (builtins.attrNames attrSet);
-      packagesList = attrsToList channelPackages;
+    getFlakeFlakePath = packageAttrPath: _:
+      [
+        "legacyPackages"
+        system
+      ]
+      ++ packageAttrPath;
+
+    # utility function - should be in lib?
+    # f is a function that takes a name and value and returns a string categorizing that name and
+    # value
+    groupAttrSetBy = f: attrSet: let
+      listWithKeys =
+        lib.mapAttrsToList (name: value: let
+          groupByString = f name value;
+        in {
+          "${groupByString}" = {"${name}" = value;};
+        })
+        attrSet;
+    in
+      # we have [{groupByString = {name = value};}], and we know every name is unique, so combine
+      # all attribute sets with the same groupByString
+      builtins.zipAttrsWith (
+        _: values:
+          builtins.foldl'
+          lib.recursiveUpdate
+          {}
+          values
+      )
+      listWithKeys;
+
+    groupedChannels =
+      groupAttrSetBy (
+        channelName: _:
+          if lib.isStorePath channelName
+          then "storePaths"
+          else let
+            fetchedChannel = builtins.getFlake channelName;
+          in
+            if builtins.hasAttr "evalCatalog" fetchedChannel
+            then "channels"
+            else "flakes"
+      )
+      config.packages;
+
+    # partially apply generateFakeCatalog to the appropriate getters
+    packagesWithDerivation =
+      builtins.concatLists (lib.mapAttrsToList (getDerivationsForPackages getFlakeCatalogPath getFlakeFlakePath) groupedChannels.flakes)
+      ++ builtins.concatLists (lib.mapAttrsToList (getDerivationsForPackages getChannelCatalogPath getChannelFlakePath) groupedChannels.channels);
+    storePaths =
+      if groupedChannels ? storePaths
+      then builtins.attrNames groupedChannels.storePaths
+      else [];
+
+    getDerivationsForPackages = catalogPathGetter: flakePathGetter: channelName: channelPackages: let
+      # in order to support nested packages, we have to recurse until no attributes are attribute
+      # sets, or there is a "config" attribute set
+      isNotPackageConfig = attrs: ! attrs ? "config" && builtins.any (value: builtins.isAttrs value) (builtins.attrValues attrs);
+      # extract a list from the nested configuration format
+      # return list of a packagesAttrSets where a packageAttrSet is of the form
+      # {
+      #   attrPath = ["python3Packages" "requests"];
+      #   packageConfig = {
+      #     config = {};
+      #     version = "1.12";
+      #   };
+      # };
+      packageAttrSetsList = lib.collect (attrs: attrs ? attrPath && attrs ? packageConfig) (lib.mapAttrsRecursiveCond isNotPackageConfig (attrPath: value: {
+          inherit attrPath;
+          packageConfig = value;
+        })
+        channelPackages);
+
+      # parition packages based on whether they are already in the catalog
       partitioned =
         builtins.partition (
           packageAttrSet:
-            lib.hasAttrByPath (getCatalogPath channelName packageAttrSet.name packageAttrSet.value) catalog
+            lib.hasAttrByPath (catalogPathGetter channelName packageAttrSet.attrPath packageAttrSet.value) catalog
         )
-        packagesList;
+        packageAttrSetsList;
+
       alreadyInCatalog =
         builtins.map (
           packageAttrSet: let
-            catalogPath = getCatalogPath channelName packageAttrSet.name packageAttrSet.value;
-          in
-            # channelName will be added elsewhere
-            lib.setAttrByPath (builtins.tail catalogPath) (
-              self.lib.mkFakeDerivation (lib.getAttrFromPath catalogPath catalog)
-            )
+            catalogPath = catalogPathGetter channelName packageAttrSet.attrPath packageAttrSet.packageConfig;
+          in rec {
+            derivation =
+              self.lib.mkFakeDerivation (lib.getAttrFromPath catalogPath catalog);
+            catalogData = derivation.meta.element;
+            inherit (packageAttrSet) attrPath;
+            inherit channelName catalogPath;
+            flakePath = flakePathGetter packageAttrSet.attrPath packageAttrSet.packageConfig;
+          }
         )
         partitioned.right;
       fromChannel = let
-        fetchedChannel = let
-          # TODO tryEval doesn't actually catch this
-          tryFetchChannel = builtins.tryEval (builtins.getFlake channelName);
-        in
-          if tryFetchChannel.success
-          then tryFetchChannel.value
-          else throw "No channel ${channelName} found in channel subscriptions; you can subscribe to it with \"flox subscribe\"";
-
-        channelEvalCatalog =
-          if builtins.hasAttr "evalCatalog" fetchedChannel
-          then fetchedChannel.evalCatalog
-          else throw "Channel ${channelName} does not contain a catalog";
+        fetchedFlake = builtins.getFlake channelName;
       in
         builtins.map (
           packageAttrSet: let
-            catalogPath = getCatalogPath channelName packageAttrSet.name packageAttrSet.value;
-          in let
-            catalogPathWithoutChannel = builtins.tail catalogPath;
-            evalCatalogPath = getEvalCatalogPath packageAttrSet.name packageAttrSet.value;
+            catalogPath = catalogPathGetter channelName packageAttrSet.attrPath packageAttrSet.packageConfig;
+            flakePath = flakePathGetter packageAttrSet.attrPath packageAttrSet.packageConfig;
           in
             # this function returns just the entries for this channel, and the caller adds channelName to the complete catalog
-            lib.setAttrByPath catalogPathWithoutChannel (
-              if lib.hasAttrByPath evalCatalogPath channelEvalCatalog
-              then lib.getAttrFromPath evalCatalogPath channelEvalCatalog
-              else throw "Channel ${channelName} does not contain ${builtins.concatStringsSep "." catalogPathWithoutChannel}"
-            )
+            rec {
+              derivation =
+                if lib.hasAttrByPath flakePath fetchedFlake
+                then lib.getAttrFromPath flakePath fetchedFlake
+                else throw "Channel ${channelName} does not contain ${builtins.concatStringsSep "." flakePath}";
+              catalogData =
+                if derivation ? meta.element
+                then derivation.meta.element
+                # todo readPackage
+                else null;
+              inherit (packageAttrSet) attrPath;
+              inherit channelName catalogPath flakePath;
+            }
         )
         partitioned.wrong;
     in
-      builtins.foldl'
-      lib.recursiveUpdate
-      {}
-      (alreadyInCatalog ++ fromChannel);
+      alreadyInCatalog ++ fromChannel;
 
     # we could check uniqueness in O(n log n) by first sorting all elements by storePaths, but I don't think that's
     # worth my time at the moment
-    uniqueFakeCatalog =
-      lib.mapAttrsRecursiveCond notDerivation (
-        attrPath1: fakeDerivation1:
-        # we need to throw if fakeDerivation1 is not unique, but if it is unique, we don't need the
+    # instead, for every package, compare to every package and every store path
+    uniquePackagesWithDerivation =
+      builtins.map (
+        packageWithDerivation1:
+        # we need to throw if packageWithDerivation1 is not unique, but if it is unique, we don't need the
         # result of this computation, so use deepSeq
-          builtins.deepSeq (lib.mapAttrsRecursiveCond notDerivation (
-              attrPath2: fakeDerivation2:
-                if attrPath1 == attrPath2
+        # compare against all packages from flakes and channels
+          builtins.deepSeq (builtins.map (
+              packageWithDerivation2:
+                if packageWithDerivation1.attrPath == packageWithDerivation2.attrPath
                 then null
-                else if
-                  (builtins.sort builtins.lessThan fakeDerivation1.meta.element.element.storePaths)
-                  == (builtins.sort builtins.lessThan fakeDerivation2.meta.element.element.storePaths)
-                then throw "package ${builtins.concatStringsSep "." attrPath1} is identical to package ${builtins.concatStringsSep "." attrPath2}"
+                # TODO compare against flake packages for uniqueness
+                else if packageWithDerivation1.catalogData != null && packageWithDerivation2.catalogData != null
+                then
+                  if
+                    (builtins.sort builtins.lessThan packageWithDerivation1.catalogData.element.storePaths)
+                    == (builtins.sort builtins.lessThan packageWithDerivation2.catalogData.element.storePaths)
+                  then throw "package ${builtins.concatStringsSep "." packageWithDerivation1.attrPath} is identical to package ${builtins.concatStringsSep "." packageWithDerivation2.attrPath}"
+                  else null
                 else null
             )
-            newFakeCatalog)
-          fakeDerivation1
+            packagesWithDerivation)
+          # compare against storePaths
+          (builtins.deepSeq
+            (builtins.map (
+                storePath:
+                # TODO compare against flake packages for uniqueness
+                  if packageWithDerivation1.catalogData != null && packageWithDerivation1.catalogData.element.storePaths == [storePath]
+                  then throw "package ${builtins.concatStringsSep "." packageWithDerivation1.attrPath} is identical to store path ${storePath}"
+                  else null
+              )
+              storePaths)
+            # pass packageWithDerivation1 through the map - in other words, do nothing
+            packageWithDerivation1)
       )
-      newFakeCatalog;
+      packagesWithDerivation;
 
-    # extract a list of fake derivations
+    # extract a list of derivations
     packagesList =
-      lib.collect lib.isDerivation uniqueFakeCatalog;
+      builtins.map (packageWithDerivation: packageWithDerivation.derivation) uniquePackagesWithDerivation
+      ++ storePaths;
 
-    # get rid of the fake derivations; we just need meta.element
+    # store paths are not added to the catalog
     newCatalog =
-      lib.mapAttrsRecursiveCond notDerivation
-      (_: fakeDerivation: fakeDerivation.meta.element or "this catalog doesn't add element to meta")
-      uniqueFakeCatalog;
-    # turn uniqueFakeCatalog into a backwards compatible manifest.json
+      builtins.foldl' lib.recursiveUpdate {}
+      (builtins.map
+        (packageWithDerivation:
+          lib.setAttrByPath
+          packageWithDerivation.catalogPath
+          packageWithDerivation.catalogData)
+        packagesWithDerivation);
+
+    # For flake:
+    # {
+    #   "active": true,
+    #   "attrPath": "legacyPackages.aarch64-darwin.hello",
+    #   "originalUrl": "flake:nixpkgs",
+    #   "outputs": null,
+    #   "priority": 5,
+    #   "storePaths": [
+    #     "/nix/store/gq5b6y0zxvpfxywi600ahlcg3mnscv93-hello-2.12.1"
+    #   ],
+    #   "url": "github:flox/nixpkgs/2788904d26dda6cfa1921c5abb7a2466ffe3cb8c"
+    # }
+
+    # For channel:
+    # {
+    #   "active": true,
+    #   "attrPath": "evalCatalog.aarch64-darwin.stable.hello",
+    #   "originalUrl": "flake:nixpkgs-flox",
+    #   "outputs": null,
+    #   "priority": 5,
+    #   "storePaths": [
+    #     "/nix/store/lc9cci22gfxd7xaqjdvz3kkd09g4g0g7-hello-2.12.1"
+    #   ],
+    #   "url": "github:flox/nixpkgs-flox/5cdedb611cf745c734b0268346e940a8b1e33b45"
+    # },
+    packageManifestElements = builtins.map (packageWithDerivation:
+      {
+        active = true;
+        attrPath = lib.concatStringsSep "." packageWithDerivation.flakePath;
+        # TODO conditionally add flake:
+        originalUrl = "flake:${packageWithDerivation.channelName}";
+      }
+      // (
+        if packageWithDerivation.catalogData != null
+        then {
+          inherit (packageWithDerivation.catalogData.element) url outputs storePaths;
+        }
+        # TODO readPackage
+        else {
+          url = "url";
+          outputs = null;
+          storePaths = ["storePath"];
+        }
+      ))
+    packagesWithDerivation;
+    storePathManifestElements =
+      builtins.map (storePath: {
+        active = true;
+        storePaths = [
+          storePath
+        ];
+      })
+      storePaths;
+
     manifestJSON = builtins.toJSON {
       version = 2;
-      elements =
-        lib.flatten
-        (lib.mapAttrsToList (
-            channel: systemPackages:
-              lib.mapAttrsToList (system: stabilityPackages:
-                lib.mapAttrsToList (
-                  stability: packagePackages:
-                    lib.mapAttrsToList (
-                      package: versionPackages:
-                        lib.mapAttrsToList (
-                          version: fakeDerivation: {
-                            inherit (fakeDerivation.meta.element.element) url outputs storePaths;
-                            active = true;
-                            attrPath = lib.concatStringsSep "." ["evalCatalog" system stability package];
-                            originalUrl = "flake:${channel}";
-                          }
-                        )
-                        versionPackages
-                    )
-                    packagePackages
-                )
-                stabilityPackages)
-              systemPackages
-          )
-          uniqueFakeCatalog);
+      elements = packageManifestElements ++ storePathManifestElements;
     };
   in {
     manifestPath = builtins.toFile "profile" manifestJSON;
