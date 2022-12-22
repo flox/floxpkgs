e92f903950382e78382aaa5e085e49e39789f8c5
Tom Bereknyei <tomberek@gmail.com>
2022-12-22 11:10:01 -0500

    chore!: renaming repos

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   # Declaration of external resources
   # =================================
 
-  inputs.flox.url = "git+ssh://git@github.com/flox/flox?ref=main";
+  inputs.flox.url = "git+ssh://git@github.com/flox/flox-bash?ref=main";
   inputs.flox.inputs.floxpkgs.follows = "/";
 
   inputs.tracelinks.url = "git+ssh://git@github.com/flox/tracelinks?ref=fix/overlay-trees";
