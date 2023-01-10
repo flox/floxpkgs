3bd5aaf766440bf282bfb0662cb706943bead78a
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2023-01-10 12:02:59 -0600

    feat: support evaluated env vars
    This allows env vars to depend on state at activation time

--- modules/options.nix
+++ modules/options.nix
@@ -13,15 +13,35 @@ with lib; {
         VISUAL = "nvim";
       };
       description = lib.mdDoc ''
-        A set of environment variables. The value of each variable can be either a string or a list of
-        strings.  The latter is concatenated, interspersed with colon
-        characters.
+        A set of environment variables. The value of each variable can be either
+        a string or a list of strings.  The latter is concatenated, interspersed
+        with colon characters.
+
+        Alternatively, this may be a list of sets of environment variables. In
+        that case, order of the variables is preserved, and values are not
+        escaped, which means variables may be evaluated at runtime.
       '';
-      type = with types; attrsOf (either str (listOf str));
-      apply = mapAttrs (n: v:
-        if isList v
-        then concatStringsSep ":" v
-        else v);
+      type = with types; let
+        envAttrSet =
+          # {
+          #   FOO = "BAR";
+          #   PATH = ["/bin" "/usr/bin"];
+          # }
+          attrsOf (either str (listOf str));
+      in
+        either envAttrSet (listOf envAttrSet);
+      apply = let
+        joinWithColon = envAttrSet:
+          mapAttrs (n: v:
+            if isList v
+            then concatStringsSep ":" v
+            else v)
+          envAttrSet;
+      in
+        definition:
+          if builtins.isList definition
+          then builtins.map joinWithColon definition
+          else joinWithColon definition;
     };
 
     toplevel = mkOption {
