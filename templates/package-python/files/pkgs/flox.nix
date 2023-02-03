b641c0298551408afef7a82963c404011fa75f8f
Rok Garbas <rok@garbas.si>
2023-02-03 01:17:24 +0100

    another typo

--- templates/package-python/files/pkgs/flox.nix
+++ templates/package-python/files/pkgs/flox.nix
@@ -1,7 +1,7 @@
 {
   # flox environment configuation
   #
-  # flake.nix options: https://floxdev.com/docs/reference/flox-nix-config
+  # flox.nix options: https://floxdev.com/docs/reference/flox-nix-config
   # Getting started with flox: https://floxdev.com/docs
   # Get help: https://discourse.floxdev.com
   #
