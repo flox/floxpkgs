ab640e052a262b0f0ae2450dbb6b0ac56bf17b03
Sam Rose <sam@floxdev.com>
2023-02-06 14:47:01 -0500

    fix: remove lib etc

--- templates/package-python/files/pkgs/default.nix
+++ templates/package-python/files/pkgs/default.nix
@@ -1,16 +1,15 @@
 {
   self,
   python3Packages,
-  lib,
 }:
 python3Packages.buildPythonPackage {
   pname = "my-package";
-  version = "0.0.0-${lib.flox-floxpkgs.getRev self}";
+  version = "0.0.0";
   src = self;
   PIP_DISABLE_PIP_VERSION_CHECK = 1;
   # Add Python modules needed by your package here
   propagatedBuildInputs = with python3Packages; [
     requests
   ];
-  meta.description = "An example of flox package."
+  meta.description = "An example of flox package.";
 }
