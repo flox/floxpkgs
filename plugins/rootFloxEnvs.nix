4afc7f37f06f1a93f471cde5392e5e9cbef7440c
Tom Bereknyei <tomberek@gmail.com>
2023-03-02 01:02:50 -0500

    fix: add position information to floxEnvs

--- plugins/rootFloxEnvs.nix
+++ plugins/rootFloxEnvs.nix
@@ -2,21 +2,24 @@
   lib,
   self,
   ...
-}: {
-  injectedArgs ? {},
-}: {context, ...}: let
+}: {injectedArgs ? {}}: {context, ...}: let
   floxEnvDir = context.self.outPath;
   floxNixPath = "${floxEnvDir}/flox.nix";
   catalogPath = "${floxEnvDir}/catalog.json";
   namespace = ["default"];
   result = lib.genAttrs context.systems (system: {
-    default = self.lib.mkFloxEnv {
+    default = lib.recursiveUpdate (self.lib.mkFloxEnv {
       inherit context system namespace;
       modules = [floxNixPath] ++ lib.optional (builtins.pathExists catalogPath) {inherit catalogPath;};
-    };
+    }) {meta.position = builtins.unsafeDiscardStringContext floxNixPath;};
   });
-in
-  {
-    floxEnvs = if builtins.pathExists floxNixPath then result else {};
-    devShells = if builtins.pathExists floxNixPath then result else {};
-  }
+in {
+  floxEnvs =
+    if builtins.pathExists floxNixPath
+    then result
+    else {};
+  devShells =
+    if builtins.pathExists floxNixPath
+    then result
+    else {};
+}
