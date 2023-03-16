66340b8c3e1f4a45eef8e8f59214389ffc510e35
Rok Garbas <rok@floxdev.com>
2023-03-16 20:43:48 +0100

    Update flox.nix

--- templates/project/files/flox.nix
+++ templates/project/files/flox.nix
@@ -1,9 +1,27 @@
 {
   # flox environment configuration
   #
+  # TODO: 1min nix language guide to be able to write/read flox.nix
   # flox.nix options: https://floxdev.com/docs/reference/flox-nix-config
   # Getting started with flox: https://floxdev.com/docs
   # Get help: https://discourse.floxdev.com
   #
   # Happy hacking!
+  
+  # Add packages from `nixpkgs-flox` catalog:
+  #
+  # packages.nixpkgs-flox.go = {};
+  # packages.nixpkgs-flox.nodejs = {}
+  #
+  # Or look for packages with `flox search` command.
+  
+  #
+  # environmentVariables.LANG = "en_US.UTF-8";
+  
+  # 
+  # shell.hook = ''
+  #   
+  # '';
+  
+  # Look at other integrations: https://floxdev.com/docs/integrations(TODO)
 }
