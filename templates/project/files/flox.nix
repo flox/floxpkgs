908f208ef19676774526e613bddc1a0b101774b4
Rok Garbas <rok@floxdev.com>
2023-03-21 15:10:52 +0100

    format a template a bit

--- templates/project/files/flox.nix
+++ templates/project/files/flox.nix
@@ -1,27 +1,32 @@
 {
-  # flox environment configuration
+  # flox environment
   #
-  # TODO: 1min nix language guide to be able to write/read flox.nix
-  # flox.nix options: https://floxdev.com/docs/reference/flox-nix-config
-  # Getting started with flox: https://floxdev.com/docs
-  # Get help: https://discourse.floxdev.com
+  # Look at the examples of configuration bellow.
+  # check the documentation:
+  #   https://floxdev.com/docs/reference/flox-nix-config
+  #
+  # To learn basics about flox commands see:
+  #   https://floxdev.com/docs/basics
+  # 
+  # Get help:
+  #   https://discourse.floxdev.com
   #
   # Happy hacking!
-  
+  #
+
   # Add packages from `nixpkgs-flox` catalog:
   #
   # packages.nixpkgs-flox.go = {};
   # packages.nixpkgs-flox.nodejs = {}
   #
-  # Or look for packages with `flox search` command.
-  
+  # Look for packages with `flox search` command.
+
+  # Set environment variables
   #
   # environmentVariables.LANG = "en_US.UTF-8";
-  
-  # 
+
+  # Run shell hook when you enter flox environment
   # shell.hook = ''
-  #   
+  #   echo "Welcome to flox environment"
   # '';
-  
-  # Look at other integrations: https://floxdev.com/docs/integrations(TODO)
 }
