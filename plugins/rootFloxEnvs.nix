b07933add38f77bf82901763af2ed11ffbded54a
Yannik Sander <yannik@floxdev.com>
2023-05-08 16:17:42 +0200

    fix: specialize root flox.nix context by system

--- plugins/rootFloxEnvs.nix
+++ plugins/rootFloxEnvs.nix
@@ -9,7 +9,8 @@
   namespace = ["default"];
   result = lib.genAttrs context.systems (system: {
     default = lib.recursiveUpdate (self.lib.mkFloxEnv {
-      inherit context system namespace;
+      inherit system namespace;
+      context = context.context' system;
       modules = [floxNixPath] ++ lib.optional (builtins.pathExists catalogPath) {inherit catalogPath;};
     }) {meta.position = builtins.unsafeDiscardStringContext floxNixPath;};
   });
