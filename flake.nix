f3a6814caabea89fe900a97867bdc55b48ad66c6
Rok Garbas <rok@floxdev.com>
2023-05-10 19:39:17 +0200

    move flox.nix to flake.nix

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
   #       overwrites flox-floxpkgs/nixpkgs/nixpkgs flake input.
   #       We should change that to `flox/nixpkgs-flox/nixpkgs`
   inputs.flox-floxpkgs.url = "git+ssh://git@github.com/flox/flox?ref=next";
+
   inputs.flox.follows = "flox-floxpkgs";
 
   inputs.tracelinks.url = "git+ssh://git@github.com/flox/tracelinks?ref=main";
@@ -22,5 +23,37 @@
   inputs.builtfilter.inputs.flox.follows = "flox-floxpkgs";
   # =================================
 
-  outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (_: {});
+  outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (
+    {
+      self,
+      inputs,
+      lib,
+      ...
+    }:
+      # re-call yourself with overrides, will not work if using in-memory lockfile
+      let hydraOverride = path: follows: (inputs.capacitor.lib.capacitor.callFlake
+        (builtins.readFile (self + "/flake.lock"))
+        self "" "" "root" { }
+        [ {
+            path = path;
+            follows = follows;
+          } ]
+        ).hydraJobs;
+      in
+# Define package set structure
+    {
+      # Limit the systems to fewer or more than default by ucommenting
+      packages = {
+        builtfilter = {capacitated, ...}: capacitated.builtfilter.packages.builtfilter-rs;
+      };
+
+      # reexport of capacitor, defaultPlugins, project, lib, templates, apps
+      #passthru = { inherit (inputs.flox) capacitor defaultPlugins project lib templates apps; };
+      #passthru.project = inputs.flox.project;
+      passthru.project = "hello";
+
+      passthru."hydraJobsStaging" = hydraOverride ["nixpkgs" "nixpkgs"] ["nixpkgs" "nixpkgs-staging"];
+      passthru."hydraJobsUnstable" = hydraOverride ["nixpkgs" "nixpkgs"] ["nixpkgs" "nixpkgs-unstable"];
+      passthru."hydraJobsStable" = hydraOverride ["nixpkgs" "nixpkgs"] ["nixpkgs" "nixpkgs-stable"];
+    });
 }
