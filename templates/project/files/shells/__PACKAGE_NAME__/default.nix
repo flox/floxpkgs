76531c45fad2e4284f5dec2553664ddb788de8d2
Matthew Kenigsberg <matthew@floxdev.com>
2023-05-15 15:58:28 -0600

    add env var and explain shellHook

--- templates/project/files/shells/__PACKAGE_NAME__/default.nix
+++ templates/project/files/shells/__PACKAGE_NAME__/default.nix
@@ -9,23 +9,27 @@
 # `packages`, `buildInputs`, and `shellHook`.
 #
 mkShell {
-  #
   # Compilers and libraries go here
-  #
   buildInputs = [
     stdenv.cc
     zlib
   ];
-  #
+
   # Add extra tools here
-  #
   packages = [
     gnumake
   ];
-  #
-  # Additional shell hooks go here
-  #
+
+  # Any variable set in this block that isn't a reserved word will be set as an
+  # environment variable in the environment.
+  WELCOME_MESSAGE = "Run make to build this project";
+
+  # A shell hook is a script to run when entering an environment.
+  # It can be used to perform any custom activation steps needed for your
+  # project.
   shellHook = ''
     make --version
+    echo
+    echo "$WELCOME_MESSAGE"
   '';
 }
