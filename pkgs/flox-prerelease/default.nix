f73a077c994d0a68fc25c3526a4bbafe198545ca
Yannik Sander <yannik@floxdev.com>
2023-07-24 17:46:06 +0200

    build: reexport flox packages as lazy derivations

--- pkgs/flox-prerelease/default.nix
+++ pkgs/flox-prerelease/default.nix
@@ -1,1 +1,31 @@
-{capacitated}: capacitated.flox-main.packages.flox
+/*
+Re-export flox as a "lazy" derivation with an additional attribute `fromCatalog`
+
+Adding an attribute to a canonical re-export such as
+
+    { capacitated }:
+      capacitated.flox-main.packages.flox // {fromCatalog = self.evalCatalog.stable.flox; }
+
+requires the evaluation of `capacitated.flox-main.packages.flox` to an attrset.
+
+We want to allow installin packages from the catalog explicitly without any evaluation of the
+upstream package (which involves fetching any number of additional flakes).
+*/
+
+{
+  self,
+  capacitated,
+  ...
+}:
+{
+  type = "derivation";
+
+  # re-export outputs and derivation attributes to allow building
+  inherit (capacitated.flox-main.packages.flox) outputs out man outPath outputName drvPath name system;
+
+  # re-export metadata
+  meta = capacitated.flox-main.packages.flox.meta;
+
+  # add additional attribute
+  fromCatalog = self.evalCatalog.stable.flox-prerelease;
+}
--- pkgs/flox/default.nix
+++ pkgs/flox/default.nix
@@ -1,1 +1,31 @@
-{capacitated, self}: capacitated.flox-latest.packages.flox // { fromCatalog = self.evalCatalog.stable.flox; }
+/*
+Re-export flox as a "lazy" derivation with an additional attribute `fromCatalog`
+
+Adding an attribute to a canonical re-export such as
+
+    { capacitated }:
+      capacitated.flox-latest.packages.flox // {fromCatalog = self.evalCatalog.stable.flox; }
+
+requires the evaluation of `capacitated.flox-latest.packages.flox` to an attrset.
+
+We want to allow installin packages from the catalog explicitly without any evaluation of the
+upstream package (which involves fetching any number of additional flakes).
+*/
+
+{
+  self,
+  capacitated,
+  ...
+}:
+{
+  type = "derivation";
+
+  # re-export outputs and derivation attributes to allow building
+  inherit (capacitated.flox-latest.packages.flox) outputs out man outPath outputName drvPath name system;
+
+  # re-export metadata
+  meta = capacitated.flox-latest.packages.flox.meta;
+
+  # add additional attribute
+  fromCatalog = self.evalCatalog.stable.flox;
+}
